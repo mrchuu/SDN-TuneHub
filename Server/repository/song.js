c4cfa8764b5e2a6c8d32b24f4869d9e90c05c394
huyngquang02-1633 <huy12042002yb@gmail.com>
2024-02-22 11:25:48 +0700

    song

--- Server/repository/song.js
+++ Server/repository/song.js
@@ -1,31 +1,44 @@
 import Song from "../model/Song.js";
 import SongStreamRepository from "./songStream.js";
-const getAllSongs = async () =>{
-    try {
-        const songList = await Song.find().populate('artist').populate('album').exec();
-        return songList;
-    } catch (error) {
-        throw new Error(error.message)
-    }
-}
-const getSongsById = async (songId) =>{
-    try {
-        const existingSong = await Song.findById(songId).populate('artist').populate('album').exec();
-        return existingSong
-    } catch (error) {
-        throw new Error(error.message)
-    }
-}
-const streamSong = async (songId, userId) =>{
-    try {
-        const songStream = await SongStreamRepository.addSongStreamm({userId:userId, songId:songId});
-        return songStream;
-    } catch (error) {
-        throw new Error(error.message)
-    }
-}
+const getAllSongs = async () => {
+  try {
+    const songList = await Song.find()
+      .populate("artist", "_id artist_name")
+      .populate("album")
+      .select("_id")
+      .select("song_name")
+      .select("artist")
+      .select("cover_image")
+      .select("duration");
+    return songList;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+const getSongsById = async (songId) => {
+  try {
+    const existingSong = await Song.findById(songId)
+      .populate("artist")
+      .populate("album")
+      .exec();
+    return existingSong;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+const streamSong = async (songId, userId) => {
+  try {
+    const songStream = await SongStreamRepository.addSongStreamm({
+      userId: userId,
+      songId: songId,
+    });
+    return songStream;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 export default {
-    getAllSongs,
-    streamSong,
-    getSongsById
-}
+  getAllSongs,
+  streamSong,
+  getSongsById,
+};
