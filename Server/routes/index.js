2492e57cc3383e0b26864132fc60022a1fd53e89
mrchuu <chuquyson4@gmail.com>
2024-02-27 23:20:50 +0700

    resolve conflict

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,6 +1,12 @@
 import AuthenticationRouter from "./authentication.js";
-import SongRouter from "./song.js"
+import SongRouter from "./song.js";
 import GenreRouter from "./genre.js";
-import ArtistRouter from "./artist.js"
+import ArtistRouter from "./artist.js";
 import UserRouter from "./user.js";
-export { AuthenticationRouter, SongRouter , UserRouter,ArtistRouter, GenreRouter};
+export {
+  AuthenticationRouter,
+  SongRouter,
+  UserRouter,
+  ArtistRouter,
+  GenreRouter,
+};
