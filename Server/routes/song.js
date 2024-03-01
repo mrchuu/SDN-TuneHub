49f6568c7805642eee1b46cb5be12ebb9e82e2d7
mrchuu <chuquyson4@gmail.com>
2024-03-01 08:24:15 +0700

    remove token verification from get all song

--- Server/routes/song.js
+++ Server/routes/song.js
@@ -2,7 +2,7 @@ import { SongController } from "../controller/index.js";
 import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
 const songRouter = express.Router();
-songRouter.get("/getAll", verifyToken, SongController.getAllSongs);
+songRouter.get("/getAll", SongController.getAllSongs);
 songRouter.get("/streamSong/:songId", SongController.streamSong);
 songRouter.post("/addSongStream/:songId", SongController.addStreamSong);
 songRouter.post("/uploadSingle", verifyToken,SongController.uploadSong);
