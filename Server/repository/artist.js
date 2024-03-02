5ed7ca716a1a0b8a3f96602d7de84b4e2585ce58
hieunthe163894 <hieunthe163894@fpt.edu.vn>
2024-03-03 00:53:54 +0700

    Hieu

--- Server/repository/artist.js
+++ Server/repository/artist.js
@@ -1,11 +1,11 @@
 import Artist from "../model/Artist.js";
 import song from "../model/Song.js";
 
-const findArtistByName = async ({searchInput, artistId}) => {
+const findArtistByName = async ({ searchInput, artistId }) => {
   try {
     return await Artist.find({
       artist_name: { $regex: new RegExp(`.*${searchInput}.*`, "i") },
-      _id: {$ne: artistId}
+      _id: { $ne: artistId }
     }).exec();
   } catch (error) {
     throw new Error(error.message);
@@ -189,20 +189,8 @@ const hotArtist = async () => {
           "artist_file.profile_picture": 1,
           artist_followed_count: { $size: "$artist_file.artist_followed" },
         },
-      },
-        {
-          $limit: 5
-        },
-        {
-          $project: {
-            _id: 1,
-            artist_name: 1,
-            "artist_file.introduction": 1,
-            "artist_file.profile_picture": 1,
-            "artist_followed_count": { $size: "$artist_file.artist_followed" }
-          }
-        }
-      ]
+      }
+    ]
     ).exec();
     return result;
   } catch (error) {
