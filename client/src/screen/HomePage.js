992c6bb6389d5e3abb5406316a1e8a509f4341c8
mrchuu <chuquyson4@gmail.com>
2024-03-18 13:27:26 +0700

    :((

--- client/src/screen/HomePage.js
+++ client/src/screen/HomePage.js
@@ -1,23 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import DefaultTemplate from "../template/DefaultTemplate";
-import SERVER_URL from "../config.js";
-import toast from "react-hot-toast";
-import { Link, useNavigate } from "react-router-dom";
-import PerformRequest from "../utilities/PerformRequest.js";
-import { useDispatch, useSelector } from "react-redux";
-import { login, logOut } from "../redux/auth.js";
-import SongList from "../component/SongList.js";
+import "../style/carousel.css";
+import SongList from "../component/SongList";
 export default function HomePage() {
-  const navigate = useNavigate();
-  const hasMounted = useRef(false);
-  const auth = useSelector((state) => state.auth.userInfo);
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const dispatch = useDispatch();
-  const { OriginalRequest } = PerformRequest();
   return (
     <DefaultTemplate>
       <div className="w-full min-h-screen">
-        <SongList/>
+        <SongList />
       </div>
     </DefaultTemplate>
   );
